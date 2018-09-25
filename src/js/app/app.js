512daceae35391b9ad79428e5764aa9f9afcef3a
Andrew Archbold <pulsegrenade@gmail.com>
2018-09-24 22:15:48 -0700

    #time 1h 0m getting header parallaxing working.

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -5,4 +5,25 @@ $.fn.exampleFunction = function() {
 $(function(){
     console.log('Hello Marketing!');
     console.log($);
+
+    var $win = $(window);
+
+    $('div.home-parallax').each(function(){
+        console.log($(this));
+        var scroll_speed = 3;
+        if ($(this).hasClass('header-bg-4')) {
+            scroll_speed = 4;
+        } else if ($(this).hasClass('header-bg-3')) {
+            scroll_speed = 5;
+        } else if ($(this).hasClass('header-bg-2')) {
+            scroll_speed = 6;
+        }
+        var $this = $(this);
+        $(window).scroll(function() {
+            var bgScroll = (($win.scrollTop() - $this.offset().top)/ scroll_speed);
+            var layerPosition = bgScroll + 'px';
+            //var bgPosition = '20% '+ bgScroll + 'px';
+            $this.css({ transform: 'translateY(' + layerPosition + ')' });
+        });
+    });
 });
