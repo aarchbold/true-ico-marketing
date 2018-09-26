2cc6f721fd9b92c4cb03629292c6f244de82e0a2
Andrew Archbold <pulsegrenade@gmail.com>
2018-09-25 23:05:59 -0700

    #time 2h 0m adding countdown timer and cleaning up parallaxer

--- src/js/app/countdown.js
+++ src/js/app/countdown.js
@@ -1,32 +1,3 @@
-$.fn.exampleFunction = function() {
-    
-}
-
-$(function(){
-    console.log('Hello Marketing!');
-    console.log($);
-
-    var $win = $(window);
-
-    $('div.home-parallax').each(function(){
-        console.log($(this));
-        var scroll_speed = 3;
-        if ($(this).hasClass('header-bg-4')) {
-            scroll_speed = 4;
-        } else if ($(this).hasClass('header-bg-3')) {
-            scroll_speed = 5;
-        } else if ($(this).hasClass('header-bg-2')) {
-            scroll_speed = 6;
-        }
-        var $this = $(this);
-        $(window).scroll(function() {
-            var bgScroll = (($win.scrollTop() - $this.offset().top)/ scroll_speed);
-            var layerPosition = bgScroll + 'px';
-            //var bgPosition = '20% '+ bgScroll + 'px';
-            $this.css({ transform: 'translateY(' + layerPosition + ')' });
-        });
-    });
-});
 $.fn.initCountdown = function() {
     var $context = $(this),
         $counterDays = $('#counterDays',$context),
